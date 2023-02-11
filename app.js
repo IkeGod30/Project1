0669a26d42789bc7fdea8bc8af1148762e6a6ac8
Anthony Izuogu <anthoizu@yahoo.com>
2023-02-11 10:53:58 -0500

    Updated all files with corrections

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 var contactForm = document.querySelector('.cont');
 var btnClass = document.querySelector('.button');
-var formShow = document.querySelector('.btn');
+//var formShow = document.querySelector('.btn');
 
 
 
 function showContact() {
-
-// var contactForm = document.querySelector('.cont');
-// const btnClass = document.querySelector('.button');
-// var formShow = document.querySelector('.btn');
-contactForm.style.visibility = 'visible';
+    contactForm.style.visibility = 'visible';
 
 }
 
